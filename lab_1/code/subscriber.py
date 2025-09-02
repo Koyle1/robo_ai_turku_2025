fb240b66b1b2f76584f254ad5f48d7dd9f1ccd03
ci72buri <ci72buri@clara02.sc.uni-leipzig.de>
2025-09-02 07:31:16 +0200

    changed folder structure

---
+++ lab_1/code/subscriber.py
@@ -0,0 +1,41 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+from rclpy.qos import QoSProfile, ReliabilityPolicy, DurabilityPolicy, HistoryPolicy
+
+class MinimalSubscriber(Node):
+
+    def __init__(self):
+        super().__init__('subscriber')
+        topic_name = '/---' # TODO: # subscribe to the publisher’s topic.
+        
+        self.qos_profile = QoSProfile(
+            reliability=ReliabilityPolicy.RELIABLE,         # Ensure delivery of messages
+            durability=DurabilityPolicy.TRANSIENT_LOCAL,    # Keep messages even if the publisher dies
+            history=HistoryPolicy.KEEP_LAST,                # Only store the last N messages
+            depth=10                                        # The number of messages to store (N=10)
+        )
+
+        self.subscription = self.create_subscription(
+            String,
+            topic_name,
+            self.listener_callback,
+            self.qos_profile)
+        
+        self.subscription
+
+
+    def listener_callback(self, msg):
+        self.get_logger().info(f'I heard: {msg.data}')
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    minimal_subscriber = MinimalSubscriber()
+    rclpy.spin(minimal_subscriber)
+    minimal_subscriber.destroy_node()
+    rclpy.shutdown()
+    
+
+if __name__ == '__main__':
+    main()
